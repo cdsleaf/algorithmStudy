57930e1d395bc1e1204a3e1d09f79b19d8621f5d
daesang.cho <daesang.cho@samsung.com>
2016-12-19 12:00:47 +0900

    add algospot template

---
+++ src/algorithm/algospot/Main.java
@@ -0,0 +1,25 @@
+package algorithm.algospot; //algospot에 제출 시 package 삭제 필요 !!
+
+import java.util.Scanner;
+
+public class Main { // class 명은 반드시 Main 으로.
+
+	static int T, N; //T는 전체 테스트 케이스의 수, N은 개별 테스트 케이스내에서 입력받는 데이터 수.
+	
+	public static void main(String[] args) throws Exception {
+
+		// System.setIn(new FileInputStream("C:/Users/SDS/git/algorithmStudy/input_txt/sds/nqueen.txt"));
+
+		Scanner sc = new Scanner(System.in);
+		T = sc.nextInt();
+		for (int test_case = 0; test_case < T; test_case++) {
+			N = sc.nextInt();
+
+			for (int i = 0; i < N; i++) {
+
+			}
+
+			//System.out.println(queenCnt);
+		}
+	}
+}
