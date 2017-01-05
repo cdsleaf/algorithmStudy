7472270b3f929786f6d1f27f549086e89ec85bae
daesang.cho <daesang.cho@samsung.com>
2017-01-05 19:24:04 +0900

    solved picnic

--- src/algorithm/algospot/exhaustiveSearch/Picnic.java
+++ src/algorithm/algospot/exhaustiveSearch/Picnic.java
@@ -20,8 +20,12 @@ import java.util.Scanner;
 public class Picnic {
 	
 	static int T, //전체 테스트 케이스 수 
-			N; //각 케이스 별 학생 수
-			
+			N, //각 케이스 별 학생 수
+			fc, //친구 쌍의 수
+			fList[], //친구 쌍 배열
+			used[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //모든 테스트케이스에서 최대 학생는 10명
+			usedCount,
+			result; //가능한 조합의 수.
 
 	public static void main(String[] args) throws Exception{
 		
@@ -33,10 +37,53 @@ public class Picnic {
 		
 		for(int testCase = 0; testCase<T; testCase++){
 			
+			result = 0; //각 테스트 케이스 별 초기화.
+			usedCount = 0;
+			for(int k=0; k<10; k++){
+				used[k] = 0;
+			}
+			
+			N = sc.nextInt();
+			fc = sc.nextInt();
+			
+			fList = new int[fc*2];
+			for(int i=0; i<fc*2; i++){
+				fList[i] = sc.nextInt();
+			}
+			
+			searchF(0);
+			
+			System.out.println(result);
+		}		
+	}
+	
+	public static void searchF(int startIndex){
+		
+		int a,b;
+		
+		if(usedCount==N){
+			result++;
+			return;
 		}
 		
-		
-		System.out.println();
+		//먼저 startIndex와 startIndex+1 의 학생이 이미 선택되었는지 체크한다.
+		//이미 선택되었다면 startIndex+2 하여 다음 친구쌍을 찾는다.
+		for(int j=startIndex; j<fc*2; j+=2){
+			a=fList[j];
+			b=fList[j+1];		
+			if(used[a]==0 && used[b]==0){
+				
+				used[a] = 1;
+				used[b] = 1;
+				usedCount+=2;
+				
+				searchF(j+2);
+				
+				used[a] = 0;
+				used[b] = 0;
+				usedCount-=2;
+				
+			}
+		}
 	}
-
 }
