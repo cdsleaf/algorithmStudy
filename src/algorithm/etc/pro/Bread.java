4b72fdf683483f9aea663ecb2ae49f41f8824f8d
daesang.cho <daesang.cho@samsung.com>
2017-05-24 15:16:40 +0900

    .

--- src/algorithm/etc/pro/Bread.java
+++ src/algorithm/etc/pro/Bread.java
@@ -13,18 +13,20 @@ import java.util.*;
 public class Bread {
 
     static int T  //전체 경우의수
-                ,N //붕어빵 기계의 수
-                ,M //발전기의 수. 1번부터 M번까지 있음.
-                ,K //기간
-                ,m1 ,m2 ,m3 ,startPoint //각 기계에 있는 발전기 번호들과 마지막엔 기계가 움직이는 에너지량
-
-    ;
+            ,N //붕어빵 기계의 수
+            ,M //발전기의 수. 1번부터 M번까지 있음.
+            ,K //기간
+            ,m1 ,m2 ,m3 ,startPoint //각 기계에 있는 발전기 번호들과 마지막엔 기계가 움직이는 에너지량
+            ,index //인덱스.
+            ;
     static long sum //최종 결과값
-                ,midSum //중간계산과정에 사용할 중간 합
-                ,K_sum //기간의 총 합. k(k+1)/2 여기에서 미리 100000123 이걸로 나누기 해야함.
-                ,fixedValue = 100000123;
-    static Integer daily[] = new Integer[100000]; // 일자별 에너지가 증가하는 발전기의 번호 를 넣어둘 변수
+            ,K_sum //기간의 총 합. k(k+1)/2 여기에서 미리 100000123 이걸로 나누기 해야함.
+            ,fixedValue = 100000123;
     static Integer num[] = new Integer[100000];
+    static Integer daily[] = new Integer[100000]; // 일자별 에너지가 증가하는 발전기의 번호 를 넣어둘 변수
+    static HashMap<Integer, Integer> energyByGenerator = new HashMap<>();
+    static ArrayList<HashMap<Integer, Integer>> dailyEnergyByGenerator= new ArrayList<>();
+    static HashMap<Integer, Integer> midIndexMap; //compare 내부에서 사용할 임시 map 변수
 
     public static void main(String[] args) throws Exception {
 
@@ -34,19 +36,15 @@ public class Bread {
 
                 long subSum=0;
 
-                for(int i=0; i<= mid; i++){
-                    if(daily[i] == m1 || daily[i] == m2 || daily[i] == m3) subSum = subSum+1;
+                subSum = calculateDailyEnergyByGenerator(mid);
 
-                }
-                //System.out.println("aaa1:"+mid+":"+subSum+":"+keyValue);
                 if(subSum > keyValue) return 1;
                 else if(subSum < keyValue) return -1;
                 else return 0;
-
             }
         };
 
-        System.setIn(new FileInputStream("./input_txt/etc/bread.txt"));
+        System.setIn(new FileInputStream("./input_txt/etc/bread_input.txt"));
 
         Scanner sc = new Scanner(System.in);
 
@@ -55,6 +53,8 @@ public class Bread {
         for(int i=0; i<T; i++){
 
             sum = 0; //최종값 초기화.
+            energyByGenerator.clear();
+            dailyEnergyByGenerator.clear();
 
             N = sc.nextInt();
             M = sc.nextInt();
@@ -64,8 +64,15 @@ public class Bread {
             K_sum = (((K*K)%fixedValue)+K)/2;
 
             for(int j=0; j<K; j++){
-                daily[j] = sc.nextInt();
+                int temp = sc.nextInt();
                 num[j] = j;
+                if(energyByGenerator.containsKey(temp)){
+                    energyByGenerator.put(temp, energyByGenerator.get(temp)+1);
+                }else{
+                    energyByGenerator.put(temp, 1);
+                }
+
+                dailyEnergyByGenerator.add((HashMap<Integer, Integer>)energyByGenerator.clone());
             }
 
             for(int p=0; p<N; p++){
@@ -75,13 +82,36 @@ public class Bread {
                 m3 = sc.nextInt();
                 startPoint = sc.nextInt();
 
-                midSum = Arrays.binarySearch(num, 0,  K, startPoint, c);System.out.println("ㄴㄴ "+midSum);
-                if(midSum >= 0){
-                    sum = sum + K_sum - ((midSum*midSum)%fixedValue+midSum)/2;
+                index =  Arrays.binarySearch(num,0, K, startPoint, c);
+
+                if(index > -1){
+
+                    while (index > 0) {
+                        if (calculateDailyEnergyByGenerator(index - 1) == calculateDailyEnergyByGenerator(index)) {
+                            index--;
+                        } else {
+                            break;
+                        }
+                    }
+
+                    sum = sum + K_sum - ((index*index)%fixedValue+index)/2;
                 }
             }
 
             System.out.println("#"+(i+1)+" "+sum);
         }
     }
+
+    private static long calculateDailyEnergyByGenerator(Integer midIndex){
+
+        long midSum = 0;
+        midIndexMap = dailyEnergyByGenerator.get(midIndex);
+
+        if(midIndexMap.containsKey(m1)) midSum = midSum + midIndexMap.get(m1);
+        if(midIndexMap.containsKey(m2)) midSum = midSum +  midIndexMap.get(m2);
+        if(midIndexMap.containsKey(m3)) midSum = midSum +  midIndexMap.get(m3);
+
+        return midSum;
+    }
+
 }
