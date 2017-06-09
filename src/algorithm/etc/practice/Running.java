056b511c5d73df17b1cb34da76df65c203c79a0e
daesang.cho <daesang.cho@samsung.com>
2017-06-09 20:14:50 +0900

    add running

--- src/algorithm/etc/practice/Running.java
+++ src/algorithm/etc/practice/Running.java
@@ -1,7 +1,8 @@
 package algorithm.etc.practice;
 
 import java.io.FileInputStream;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /*
@@ -18,7 +19,8 @@ public class Running {
 
     static int T // 전체 케이스
             ,N; //달리기 선수의 수
-    static int result; //결과값
+    static long result; //결과값
+    static List<Integer> inputRunners = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
 
@@ -31,15 +33,90 @@ public class Running {
         for(int i=0; i<T; i++){
 
             result = 0;
+            inputRunners.clear();
             N = sc.nextInt();
 
             for(int j=0; j<N; j++){
-
+                inputRunners.add(sc.nextInt());
             }
 
-
+            mergeSort(inputRunners);
 
             System.out.println("#"+(i+1)+" "+result);
         }
+    }
+
+    public static List<Integer> mergeSort(List<Integer> runners){
+
+        if(runners.size() < 2) return runners;
+
+        final List<Integer> leftList = runners.subList(0, runners.size()/2);
+        final List<Integer> rightList = runners.subList( runners.size()/2, runners.size());
+
+        return merge(mergeSort(leftList), mergeSort(rightList));
+    }
+
+    public static List<Integer> merge(List<Integer> left, List<Integer> right){
+
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int duplicatedCount = 0;
+        List<Integer> calculList = new ArrayList<>(left.size());
+        List<Integer> returnList = new ArrayList<>(left.size()+right.size());
+
+        while(left.size() > leftIndex && right.size() > rightIndex){
+
+            if(left.get(leftIndex) < right.get(rightIndex)) {
+                calculList.add(left.get(leftIndex));
+                returnList.add(left.get(leftIndex));
+                leftIndex++;
+            }else if(left.get(leftIndex) > right.get(rightIndex)){
+
+                duplicatedCount = calculatedResult(calculList, right, rightIndex, duplicatedCount);
+
+                returnList.add(right.get(rightIndex));
+                rightIndex++;
+            }else{
+
+                duplicatedCount = calculatedResult(calculList, right, rightIndex, duplicatedCount);
+
+                returnList.add(left.get(leftIndex));
+                calculList.add(left.get(leftIndex));
+                returnList.add(right.get(rightIndex));
+                leftIndex++;
+                rightIndex++;
+            }
+        }
+
+        while(left.size() > leftIndex){
+            returnList.add(left.get(leftIndex));
+            leftIndex++;
+        }
+
+        while(right.size() > rightIndex) {
+
+            duplicatedCount = calculatedResult(calculList, right, rightIndex, duplicatedCount);
+
+            returnList.add(right.get(rightIndex));
+            rightIndex++;
+        }
+
+        return returnList;
+    }
+
+    public static int calculatedResult(List<Integer> mergedList, List<Integer> inputList, int index, int dulpl){
+
+        if(mergedList.isEmpty()) return dulpl;
+
+        if(mergedList.get(mergedList.size()-1) != inputList.get(index)){
+            //만약 동일 능력치의 선수가 연속으로 나열된다면?
+            dulpl = 0;
+        }else{
+            dulpl++;
+        }
+
+        result += (mergedList.size() - dulpl);
+
+        return dulpl;
     }
 }
